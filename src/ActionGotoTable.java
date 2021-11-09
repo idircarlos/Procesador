c86a3f44006fbd3fc6bd193771df308fd7c68ea2
Idir Carlos Aliane Crespo <ic.aliane@alumnos.upm.es>
2021-11-09 16:12:58 +0100

    Cambios

--- src/ActionGotoTable.java
+++ src/ActionGotoTable.java
@@ -1,32 +1,38 @@
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.HashMap;
 import java.util.Iterator;
+import java.util.LinkedHashMap;
+import java.util.LinkedList;
 import java.util.Map;
 
 public class ActionGotoTable {
     
-    private Map<Integer, Map<String, String>> tabla; 
+    private Map<String, Map<String, String>> tabla; 
     
     public ActionGotoTable(){
-        tabla = new HashMap<Integer, Map<String, String>>();
-        inicializarTabla();
+        tabla = new LinkedHashMap<String, Map<String, String>>();
+        rellenarTablaAccGoTo();
     }
 
-    private void addEstado(Integer estado, String [] pares){
-        Map<String, String> valores = new HashMap<String, String>();
-        for (int i = 0; i < pares.length; i = i + 2){
-            valores.put(pares[i],pares[i+1]);
+    private void addEstado(String estado, ArrayList<String> pares){
+        Map<String, String> valores = new LinkedHashMap<String, String>();
+        for (int i = 0; i < pares.size(); i = i + 2){
+            valores.put(pares.get(i),pares.get(i+1));
         }
         tabla.put(estado, valores);
     }
 
-    public String getAccion(Integer estado, String terminal){
+    public String getAccion(String estado, String terminal){
         return tabla.get(estado).get(terminal);
     }
 
     public void printTable(){
-        Iterator<Integer> estados = tabla.keySet().iterator();
+        Iterator<String> estados = tabla.keySet().iterator();
         Iterator<String> pares;
-        Integer estado;
+        String estado;
         String terminal;
         while (estados.hasNext()){
             estado = estados.next();
@@ -41,192 +47,46 @@ public class ActionGotoTable {
         }
     }
 
+    private ArrayList<String> crearPares(String [] simbolos, String [] acciones){
+        ArrayList <String> pares = new ArrayList<>();
+        for (int i = 1; i < acciones.length; i++){
+            if (!acciones[i].equals("")){
+                if (simbolos[i].equals("coma")){
+                    pares.add(",");
+                }
+                else {
+                    pares.add(simbolos[i]);
+                }
+                
+                pares.add(acciones[i]);
+            }
+        }
+        return pares;
+    }
+
+    private void rellenarTablaAccGoTo(){
+        BufferedReader bf = null;
+		FileReader fr = null;
+        try {
+			fr = new FileReader("doc/prueba.csv");
+			bf = new BufferedReader(fr);
+            String [] simbolos = bf.readLine().split(",");
+            //System.out.println(Arrays.toString(simbolos));
+            String linea = null;
+            String [] fila;
+            String estado = null;
+            while ((linea = bf.readLine()) != null){
+                fila = linea.split(",");
+                estado = fila[0];
+                addEstado(estado,crearPares(simbolos, fila));
+            }
+        }catch(Exception e){
+
+        }
+    }
+
     private void inicializarTabla() {
-        addEstado(0, new String[] {"let","d4","id","d9",
-                                    "if","d5","for","d7",
-                                    "print","d10","input","d11",
-                                    "return","d12","function","d13",
-                                    "$","r3","S","1",
-                                    "B","2","C","6",
-                                    "F","3","F1","8"});
-                                    
-		/* Estado 1*/
-		addEstado(1, new String[] {"$", "acc"}); 
-
-		/* Estado 2*/
-		addEstado(2, new String[] {"let","d4","id","d9",
-                                    "if","d5","for","d7",
-                                    "print","d10","input","d11",
-                                    "return","d12","function","d13",
-                                    "$","r4","S","14",
-                                    "B","2","C","6",
-                                    "F","3","F1","8"});
-
-		/* Estado 3*/
-		addEstado(3, new String[] {"let","d4","id","d9",
-                                    "if","d5","for","d7",
-                                    "print","d10","input","d11",
-                                    "return","d12","function","d13",
-                                    "$","r4","S","15",
-                                    "B","2","C","6",
-                                    "F","3","F1","8"});
-
-        addEstado(4, new String[] {"int", "d17","string", "d18",
-                                    "boolean", "d19", "T", "16"});
-        
-        addEstado(5, new String[] {"(", "d20"});
-
-        addEstado(6, new String[] {"let", "r6", "id", "r6", 
-                                    "if", "r6", "for", "r6",
-                                    "}", "r6", "print", "r6",
-                                    "input", "r6", "return", "r6",
-                                    "function", "r6", "$", "r6"});
-
-        addEstado(7, new String[] {"(", "d21"}); 
-
-        addEstado(8, new String[] {"(", "d23", "F2", "22"});      
-
-        addEstado(9, new String[] {"(", "d24", "=", "d26", 
-                                    "%=", "d25"});
-
-        addEstado(10, new String[] {"(", "d27"});
-
-        addEstado(11, new String[] {"(", "d28"});
-
-        addEstado(12, new String[] {"id", "d35", ";", "r39",
-                                    "(", "d34", "-", "d33",
-                                    "entero", "d36", "cadena", "d37",
-                                    "X", "29", "E", "30",
-                                    "E1", "31", "E3", "32"});
-        
-        addEstado(13, new String[] {"id", "d38"});
-
-        addEstado(14, new String[] {"$", "r1"});
-
-        addEstado(15, new String[] {"$", "r2"});
-
-        addEstado(16, new String[] {"id", "d39"});
-
-        addEstado(17, new String[] {"id", "r35", "(", "r35"});
-
-        addEstado(18, new String[] {"id", "r36", "(", "r36"});
-
-        addEstado(19, new String[] {"id", "r37", "(", "r37"});
-
-        addEstado(20, new String[] {"id", "d35", "(", "d34",
-                                    "!", "d33", "entero", "d36",
-                                    "cadena", "d37", "E", "40",
-                                    "E1", "31", "E3", "32"});
-
-        addEstado(21, new String[] {"id", "d42", ";", "r16",
-                                    ")", "r16", "D", "41"});
-        
-        addEstado(22, new String[] {"{", "d44", "F3", "43"});
-
-        addEstado(23, new String[] {")", "r30", "int", "d17",
-                                    "string", "d18", "boolean", "d19",
-                                    "A", "45", "T", "46"});
-        
-        addEstado(24, new String[] {"id", "d35", "(", "d34",
-                                    ")", "r32", "!", "d33",
-                                    "entero", "d36", "cadena", "d37",
-                                    "L", "47", "E", "48",
-                                    "E1", "31", "E3", "32"});
-    
-        addEstado(25, new String[] {"id", "r37", "(", "r37"});
-
-
-
-
-              
 
         
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-        
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-        
-
-
-        addEstado(50, new String[] {";","d75","<","d54"});
-
-        addEstado(51, new String[] {")","d76","<","d54"});
-
-        addEstado(52, new String[] {")","d77"});
-
-        addEstado(53, new String[] {"let","r15","id","r15",
-                                    "if","r15","for","d54",
-                                    "}","r15","print","r15",
-                                    "input","r15","return","r15",
-                                    "function","r15","$","r15"});
-        
-        addEstado(54, new String[] {"id","d35","(","d34",
-                                    "!","d33","entero","d36",
-                                    "cadena","d37","E1","78",
-                                    "E3","32"});
-        
-        addEstado(55, new String[] {"id","d35","(","d34",
-                                    "!","d33","entero","d36",
-                                    "cadena","d37","E3","79"});
-                                    
-        addEstado(56, new String[] {";","r44",")","r44",
-                                    ",","r44","<","r44",
-                                    "-","r44"});
-
-        addEstado(57, new String[] {")","d80","<","d54"});
-
-        addEstado(58, new String[] {"id","d35","(","d34",
-                                    ")","r32","!","d33",
-                                    "entero","d36","cadena","d37",
-                                    "L","81","E","48",
-                                    "E1","31","E3","32"});
-        
-
     }
 }
--- src/Procesador.java
+++ src/Procesador.java
@@ -17,6 +17,7 @@ public class Procesador {
 	private static Map<String,Integer> palRes;
 	private static Map<String,Integer> TGlobal = new HashMap<String,Integer>();
 	private static ArrayList<String> ListTG = new ArrayList<String>();
+	private static String[] consecuentes = new String[51];
 	private static Map<String,Integer> TLocal;
 	private static boolean zonaDecl = false;
 	private static final int EOF=65535;
@@ -35,7 +36,7 @@ public class Procesador {
 
 		while(estadoActual<=6) {
 			if(car==EOF) {
-				return null;
+				return new Token(EOF,"$");
 			}
 			if(car == '\n') {
 				nLinea++;
@@ -75,21 +76,21 @@ public class Procesador {
 			case "A":
 				Integer pos = buscarTPR(cadena);
 				if (pos != null){
-					GenToken(pos,"");
+					token = GenToken(pos,"");
 				}
 				else if(zonaDecl == true){
 
 				}
 				else {
 					if(TGlobal.containsKey(cadena)) {
-						GenToken(11,TGlobal.get(cadena));
+						token = GenToken(11,TGlobal.get(cadena));
 					}
 					else {
 					pos = conter;
 					conter++;
 					TGlobal.put(cadena, pos);
 					ListTG.add(cadena);
-					GenToken(11,pos);
+					token = GenToken(11,pos);
 					}
 				}
 				
@@ -174,10 +175,169 @@ public class Procesador {
 		return token;
 	}
 
-	public static void AnSintactico (){
+	public static void ASintactico (){
 		Token token = ALexico();
+		String simbolo = obtenerLexema(token);
 		String accion;
-		Integer estado = 0;
+		char operacion=' ';
+		String estado = "0";
+		String antecedente; //Simbolos en el antecedente (cadena de simbolos)
+		int k = 1; //Simbolos en el consecuente (numero)
+		String consecuente; //Simbolos en el consecuente (cadena de simbolos)
+		String regla; //Regla utilizada (se vuelca en el fichero parse)
+		
+		Stack<String> pilaAsc = new Stack<String>();
+		pilaAsc.push(estado);
+		int iter = 1;
+		while (true){
+			System.out.println("-------------------ITER " + iter + "--------------------");
+			System.out.println( "1\t" +pilaAsc);
+			accion = agt.getAccion(estado, simbolo);
+			if(accion!=null){
+				operacion = accion.charAt(0); // Miramos si es desplazamiento ('d') regla ('r') aceptado ('acc') o error (' ')
+			}
+			if(operacion == 'd'){
+				estado = accion.substring(1,accion.length());
+				System.out.println("2\t" + pilaAsc);
+				pilaAsc.push(simbolo);
+				System.out.println("3\t" + pilaAsc);
+				pilaAsc.push(estado);
+				System.out.println("4\t" + pilaAsc);
+				token = ALexico();
+				simbolo = obtenerLexema(token);
+			}
+			else if (operacion == 'r'){
+				regla = accion.substring(1, accion.length());
+				String[] linea = consecuentes[Integer.parseInt(regla)].split(":");
+				System.out.println(Arrays.toString(linea));
+				k = Integer.parseInt(linea[1]);
+				antecedente = linea[0];
+				System.out.println("regla " +regla);
+				System.out.println("Antecedente  "+antecedente);
+				System.out.println("k " + k);
+				for (int i = 1; i <= 2*k; i++){
+					pilaAsc.pop();
+				}
+				estado = agt.getAccion(pilaAsc.lastElement(), antecedente);
+				System.out.println("5\t" + pilaAsc);
+				pilaAsc.push(antecedente);
+				System.out.println("6\t" + pilaAsc);
+				pilaAsc.push(estado);
+				System.out.println("7\t" + pilaAsc);
+			}
+			else if(operacion == 'a'){
+				System.out.println("Aceptado");
+				return;
+			}
+			else {
+				System.out.println("No aceptado");
+				System.exit(1);
+			}
+			iter++;
+			
+
+		}
+	}
+
+	public static String obtenerLexema (Token token){
+		String lexema = "";
+		if (token == null) return "";
+		Integer codigoToken = token.getId();
+		switch(codigoToken){
+			case 1:
+			lexema = "if";
+			break;
+			case 2:
+			lexema = "for";
+			break;
+			case 3:
+			lexema = "let";
+			break;
+			case 4:
+			lexema = "int";
+			break;
+			case 5:
+			lexema = "boolean";
+			break;
+			case 6:
+			lexema = "string";
+			break;
+			case 7:
+			lexema = "function";
+			break;
+			case 8:
+			lexema = "return";
+			break;
+			case 9:
+			lexema = "print";
+			break;
+			case 10:
+			lexema = "input";
+			break;
+			case 11:
+			lexema = "id";
+			break;
+			case 12:
+			lexema = "entero";
+			break;
+			case 13:
+			lexema = "cadena";
+			break;
+			case 14:
+			lexema = "=";
+			break;
+			case 15:
+			lexema = "%=";
+			break;
+			case 16:
+			lexema = "(";
+			break;
+			case 17:
+			lexema = ")";
+			break;
+			case 18:
+			lexema = "{";
+			break;
+			case 19:
+			lexema = "}";
+			break;
+			case 20:
+			lexema = ",";
+			break;
+			case 21:
+			lexema = ";";
+			break;
+			case 22:
+			lexema = "-";
+			break;
+			case 23:
+			lexema = "!";
+			break;
+			case 24:
+			lexema = "<";
+			break;
+			default:
+			lexema = "EOF";
+			break;
+
+		}
+		return lexema;
+	}
+
+	public static void rellenarConsecuentes (){
+		consecuentes[0] = "S1:1"; consecuentes[1] = "S:2"; consecuentes[2] = "S:2"; consecuentes[3] = "S:0";
+		consecuentes[4] = "A:3"; consecuentes[5] = "A:0"; consecuentes[6] = "B:5"; consecuentes[7] = "B:5";
+		consecuentes[8] = "B:1"; consecuentes[9] = "B:11"; consecuentes[10] = "B1:2"; consecuentes[11] = "B1:0";
+		consecuentes[12] = "C:5"; consecuentes[13] = "C:4"; consecuentes[14] = "C:4"; consecuentes[15] = "C:5";
+		consecuentes[16] = "C:5"; consecuentes[17] = "C:3"; consecuentes[18] = "D:0"; consecuentes[19] = "D:4";
+		consecuentes[20] = "D:4"; consecuentes[21] = "F:3"; consecuentes[22] = "F1:3"; consecuentes[23] = "F2:3";
+		consecuentes[24] = "F3:3"; consecuentes[25] = "G:2"; consecuentes[26] = "G:0"; consecuentes[27] = "H:1";
+		consecuentes[28] = "H:0"; consecuentes[29] = "K:4"; consecuentes[30] = "K:0"; consecuentes[31] = "L:2";
+		consecuentes[32] = "L:0"; consecuentes[33] = "Q:3"; consecuentes[34] = "Q:0"; consecuentes[35] = "T:1";
+		consecuentes[36] = "T:1"; consecuentes[37] = "T:1"; consecuentes[38] = "X:1"; consecuentes[39] = "X:0";
+		consecuentes[40] = "E:3"; consecuentes[41] = "E:1"; consecuentes[42] = "E1:3"; consecuentes[43] = "E1:1";
+		consecuentes[44] = "E3:2"; consecuentes[45] = "E3:3"; consecuentes[46] = "E3:1"; consecuentes[47] = "E3:4";
+		consecuentes[48] = "E3:1"; consecuentes[49] = "E3:1";
 	}
 
 	
@@ -192,9 +352,6 @@ public class Procesador {
 	}
 
 	private static Token GenToken(Integer id, Object val) {
-		if(val==null){
-			val = "";
-		}
 		String tokenTexto= "<"+id+","+val+">";
 		try {
 			FichToken.write(tokenTexto+ "\n");
@@ -268,9 +425,10 @@ public class Procesador {
 		rellenarTPR();
 		afd = new DeterministFiniteAutomate();
 		agt = new ActionGotoTable();
+		rellenarConsecuentes();
 		agt.printTable();
 		try {
-			fr = new FileReader("./data/input1.txt");
+			fr = new FileReader("./data/inputAS.txt");
 			bf = new BufferedReader(fr);
 		} catch (IOException e) {
 			e.printStackTrace();
@@ -283,9 +441,7 @@ public class Procesador {
 		} catch (IOException e) {
 			e.printStackTrace();
 		}
-		while(car!=EOF){
-			ALexico();
-		}
+		ASintactico();
 		imprimirTablaSimbolos();
 		try {
 			fr.close();
